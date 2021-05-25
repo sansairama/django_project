f9f9ecb8f47ec4e175e259eed76e8ff3800bd52d
sansairama <31078691+sansairama@users.noreply.github.com>
2021-05-25 11:55:43 +0530

    edited

--- learn/blog/urls.py
+++ learn/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostListView,PostDetailView
 from . import views
 urlpatterns = [
-    path('',views.home,name='blog-home'),
+    path('',PostListView.as_view(),name='blog-home'),
+     path('posts/<int:pk>',PostDetailView.as_view(),name='post_detail'), 
+     #the pk value helps us to go to the post that we want to(primary key)
     path('about/',views.about,name='blog-about')#this doesnt needed to be added to the project url page it will recoznise it automatically.
 ]
 #First argument of path is '' which signifies home.The empty path maps onto views.home.
--- learn/blog/views.py
+++ learn/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
 from .models import Post
 #some fake posts to see how things work.
 # Create your views here.
@@ -7,8 +7,18 @@ def home(request):
     context = {
       'posts':Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)#render is used if we want to pass the whole html page.
+    return render(request, 'blog/home.html',context)
+    #render is used if we want to pass the whole html page.
 
+class PostListView(ListView):
+    model=Post
+    template_name='blog/home.html'
+    #we can use our own template in class based views as we have used above.
+    #<app>/<model>_<viewtype>.html is the default naming convention.
+    context_object_name='posts'
+    ordering=['-date_posted']
+class PostDetailView(DetailView):
+    model=Post
 # this is what a user will see.We also need to map a url pattern to this view function.
 def about(request):
     return render(request, 'blog/about.html',{ 'title':'About'})
