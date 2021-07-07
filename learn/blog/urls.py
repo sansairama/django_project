ec0921f5c94d70b260df661d7e4eae0cecd3749f
sansairama <bilgramishashwat@gmail.com>
2021-07-07 04:33:58 -0700

    edited and added update and delete post

--- learn/blog/urls.py
+++ learn/blog/urls.py
@@ -3,13 +3,17 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    PostUpdateView
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
     )
 from . import views
 urlpatterns = [
     path('',PostListView.as_view(),name='blog-home'),
+    path('user/<str:username>',UserPostListView.as_view(),name='user-posts'),
      path('posts/<int:pk>',PostDetailView.as_view(),name='post_detail'),
      path('posts/new',PostCreateView.as_view(),name='post_create'),
+     path('posts/<int:pk>/delete/',PostDeleteView.as_view(),name='post_delete'),
      path('posts/<int:pk>/update/',PostUpdateView.as_view(),name='post_update'),
      #the pk value helps us to go to the post that we want to(primary key)
     path('about/',views.about,name='blog-about')#this doesnt needed to be added to the project url page it will recoznise it automatically.
--- learn/blog/views.py
+++ learn/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 #some fake posts to see how things work.
@@ -23,6 +25,17 @@ class PostListView(ListView):
     #<app>/<model>_<viewtype>.html is the default naming convention.
     context_object_name='posts'
     ordering=['-date_posted']
+    paginate_by=5
+class UserPostListView(ListView):
+    model=Post
+    template_name='blog/user_posts.html'
+    #we can use our own template in class based views as we have used above.
+    #<app>/<model>_<viewtype>.html is the default naming convention.
+    context_object_name='posts'
+    paginate_by=5
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 class PostDetailView(DetailView):
     model=Post
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -41,7 +54,15 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
         post=self.get_object()
         if self.request.user == post.author:
             return True
-        return False    
+        return False  
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post 
+    success_url = '/'
+    def test_func(self):#to make sure that the login user can delete in his own project
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False     
 # this is what a user will see.We also need to map a url pattern to this view function.
 def about(request):
     return render(request, 'blog/about.html',{ 'title':'About'})
