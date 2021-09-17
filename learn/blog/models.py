252a1c2c8f80fed9582aafd817bebf11611e5e12
sansairama <31078691+sansairama@users.noreply.github.com>
2021-09-17 23:52:18 +0530

    new

--- learn/blog/models.py
+++ learn/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User #user is needed because they will ac
 from django.urls import reverse
 
 class  Post(models.Model):
-    title=models.CharField(max_length=100)
+    title=models.CharField(max_length=120)
     content=models.TextField()
     date_posted=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
